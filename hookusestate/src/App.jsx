6f5756575ff1315823d9714f438f102f24823c04
Pratik Yelwande <yelwandepratik11@gmail.com>
2025-07-17 11:43:26 +0530

    Run When count Changes

--- hookusestate/src/App.jsx
+++ hookusestate/src/App.jsx
@@ -1,31 +1,14 @@
-import React, { useState } from "react";
+import React, { useEffect,useState } from "react";
 
 const App = () => {
-  const [fruit, setFruit] = useState("");
-  const [fruits, setFruits] = useState([]);
-  const additem = () => {
-    if (fruit.trim() === "") return;
-    setFruits([...fruits, fruit]);
-    setFruit("");
-  };
-  return (
-    <>
-      <input
-        type="text"
-        placeholder="Enter a fruit name"
-        value={fruit}
-        onChange={(e) => {
-          setFruit(e.target.value);
-        }}
-      />
-      <button onClick={additem}>Add</button>
-      <ul>
-        {fruits.map((f, i) => (
-          <li key={i}>{f}</li>
-        ))}
-      </ul>
-    </>
-  );
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    console.log(`Count changed to ${count}`);
+  }, []); // runs every time `count` changes
+
+  return <button onClick={() => setCount(count + 1)}>Click</button>;
+
 };
 
 export default App;
